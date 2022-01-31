18ccb858d5946fd8c9557144ee7337c96e883a42
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2022-01-31 19:10:58 +0700

    Add Bootstrap, Heading & Spacing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <h2>What I do Today?</h2>
-    <div class="homework">
-      <div v-for="task in tasks" v-bind:key="task.id" class="task">
-        <p>{{ task.title }}</p>
+    <div class="container">
+      <div class="row header mb-5 p-3">
+        <div class="col-md-12">
+          <h2>What I do Today?</h2>
+        </div>
+      </div>
+      <div class="row content">
+        <div class="col-md-12">
+          <div class="homework">
+            <div v-for="task in tasks" v-bind:key="task.id" class="task">
+              <p>{{ task.title }}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -18,14 +28,22 @@ export default {
         {
           id: 1,
           title: "Membersihkan Toilet",
+          status: false,
         },
         {
           id: 2,
           title: "Ngoding Flutter",
+          status: false,
         },
         {
           id: 3,
-          title: "Beli Macbook Pro",
+          title: "Beli Macbook Pro M1",
+          status: false,
+        },
+        {
+          id: 4,
+          title: "Belajar Vue.js",
+          status: false,
         },
       ],
     };
@@ -42,5 +60,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.header {
+  background: #1abc9c;
+}
+
+.header h2 {
+  margin: 0;
 }
 </style>
